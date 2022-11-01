74dc99c9b1dbd637e060a79d6926bb4ab9e48da2
Máximo Canedo <30154899delacroix@gmail.com>
2022-11-01 20:43:39 -0300

    Eliminado de las funciones de filtrado de registros.

--- include/menus/menu_muestra.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_MUESTRA_H_INCLUDED
-#define MENU_MUESTRA_H_INCLUDED
-
-/*
-    QEPD menuMuestras();
-*/
-
-#endif // MENU_MUESTRA_H_INCLUDED
--- include/menus/menu_reportes.h
+++ include/menus/menu_reportes.h
@@ -6,13 +6,11 @@ int reportesSimples();
 
 int menuReportes() {
     Item opciones[] = {
-        Item{1, "Filtrar registros", reportesSimples},
-        Item{-1, ""},
-        Item{2, "Recaudación en bebidas alcohólicas", recaudacionEnBebidasAlcoholicas},
-        Item{3, "Recaudación de impuestos (Todas las sucursales)", recaudacionAFIPTodasSucursales},
-        Item{4, "Recaudación de clientes casados", recaudacionCasados},
-        Item{5, "Sucursales que recaudaron efectivo por alcohol en la década 2010-2020", sucursalesQueRecaudaronCashPorVentaDeAlcoholEnLaDecada2010},
-        Item{6, "Cantidad de mozos que atendieron a clientes solteros un jueves", cantMozosQueAtendieronAClientesSolterosEnJueves},
+        Item{1, "Recaudación en bebidas alcohólicas", recaudacionEnBebidasAlcoholicas},
+        Item{2, "Recaudación de impuestos (Todas las sucursales)", recaudacionAFIPTodasSucursales},
+        Item{3, "Recaudación de clientes casados", recaudacionCasados},
+        Item{4, "Sucursales que recaudaron efectivo por alcohol en la década 2010-2020", sucursalesQueRecaudaronCashPorVentaDeAlcoholEnLaDecada2010},
+        Item{5, "Cantidad de mozos que atendieron a clientes solteros un jueves", cantMozosQueAtendieronAClientesSolterosEnJueves},
         Item{-1, ""},
         Item{0, "Volver al menú anterior", menuPrincipal}
     };
